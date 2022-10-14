e22d2e05e98349df98307ebe1075152bcddc0269
SalhSThai <83543012+SalhSThai@users.noreply.github.com>
2022-10-14 16:08:14 +0700

    Update Match.js

--- src/sequelize/models/Match.js
+++ src/sequelize/models/Match.js
@@ -1,16 +1,7 @@
 module.exports = (sequelize, DataTypes) => {
   const Match = sequelize.define('Match', {}, { underscored: true });
   Match.associate = (db) => {
-    Match.belongsTo(db.User, {
-      as: 'userId',
-      foreignKey: {
-        name: 'userId',
-        allowNull: false,
-      },
-      onDelete: 'RESTRICT',
-      onUpdate: 'RESTRICT',
-    });
-
+   
     Match.belongsTo(db.User, {
       as: 'firstId',
       foreignKey: {
