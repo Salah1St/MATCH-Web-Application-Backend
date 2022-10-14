caffac2fb4977b74407186865f4e67b0e540b5f9
Poramet <poramet@Poramets-MacBook-Pro.local>
2022-10-14 16:10:08 +0700

    back-register-finish

--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
-const { sequelize } = require('../src/sequelize/models');
-sequelize.sync({ alter: true });
+// const { sequelize } = require("../src/sequelize/models");
+// sequelize.sync({ alter: true });
 
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const morgan = require('morgan');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
 
-// const authRoute = require('./routes/authRoute');
+const authRoute = require("./routes/authRoute");
 // const friendRoute = require('./routes/friendRoute');
 // const postRoute = require('./routes/postRoute');
 // const userRoute = require('./routes/userRoute');
-const notFound = require('./middlewares/notFound');
-const error = require('./middlewares/error');
+const notFound = require("./middlewares/notFound");
+const error = require("./middlewares/error");
 // const authenticate = require('./middlewares/authenticate');
 
 const app = express();
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// app.use('/auth', authRoute);
+app.use("/auth", authRoute);
 
 app.use(notFound);
 app.use(error);
