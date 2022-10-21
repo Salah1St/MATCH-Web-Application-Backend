a78ffe26a9ef1cd6c02d14ac1b4ec753da2c382c
Poramet <poramet@Poramets-MacBook-Pro.local>
2022-10-21 17:06:33 +0700

    test01

--- src/sequelize/config/config.js
+++ src/sequelize/config/config.js
@@ -1,26 +1,26 @@
-require('dotenv').config()
-const {SQL_USERNAME,SQL_PASSWORD,SQL_DATABASE,SQL_HOST} = process.env
+require("dotenv").config();
+const { SQL_USERNAME, SQL_PASSWORD, SQL_DATABASE, SQL_HOST } = process.env;
 
 module.exports = {
-  "development": {
-    "username": SQL_USERNAME,
-    "password": SQL_PASSWORD,
-    "database": SQL_DATABASE,
-    "host": SQL_HOST,
-    "dialect": "mysql"
+  development: {
+    username: SQL_USERNAME,
+    password: SQL_PASSWORD,
+    database: SQL_DATABASE,
+    host: SQL_HOST,
+    dialect: "mysql",
   },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  test: {
+    username: "root",
+    password: null,
+    database: "database_test",
+    host: "127.0.0.1",
+    dialect: "mysql",
   },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+  production: {
+    username: "root",
+    password: null,
+    database: "database_production",
+    host: "127.0.0.1",
+    dialect: "mysql",
+  },
+};
