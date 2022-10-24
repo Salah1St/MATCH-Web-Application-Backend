1453d134f50b31665cbff4a19fd5cdc8e5c16dd2
Sarun <sarunz488817@gmail.com>
2022-10-24 11:03:45 +0700

    createPostCon

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -1,5 +1,4 @@
-const validator = require('validator');
-const jwt = require('jsonwebtoken');
+const cloudinary = require('../utils/cloudinary');
 
 const { Op } = require('sequelize');
 
@@ -9,10 +8,17 @@ const { Post, Like, Comment, sequelize } = require('../sequelize/models');
 
 exports.createPost = async (req, res, next) => {
   try {
-    const { text, image } = req.body;
+    const { text } = req.body;
     const userId = req.user.id;
+    let image;
 
-    if (!text || !text.trim() || !image || !image.trim()) {
+    if (req.files) {
+      image = await cloudinary.upload(req.files.image[0].path);
+    }
+    console.log('****');
+    console.log(image);
+    console.log(text);
+    if ((!text || !text.trim()) && (!image || !image.trim())) {
       throw new AppError(
         'Either image or text is required to create post',
         401
@@ -134,10 +140,30 @@ exports.deletePostById = async (req, res, next) => {
 
 exports.editPostById = async (req, res, next) => {
   try {
+    if (req.file) {
+      const image = req.user.profileImage;
+      const secureUrl = await cloudinary.upload(
+        req.files.image[0].path,
+        profileImage ? cloudinary.getPublicId(image) : undefined
+      );
+      updateValue.profileImage = secureUrl;
+      fs.unlinkSync(req.files.profileImage[0].path);
+    }
     const { updateData } = req.body;
+    const userId = req.user.id;
+    const postToUpdate = await Post.findOne({ where: { id: updateData.id } });
+    if (postToUpdate.userId !== userId) {
+      throw new AppError('Only post owner can Edit the post', 401);
+    }
     await Post.update(updateData, { where: { id: updateData.id } });
     const editPost = await User.findOne({
       where: { id: updateData.id },
+      include: [
+        {
+          model: User,
+          attributes: { exclude: 'password' },
+        },
+      ],
     });
     res.status(200).json({ editPost });
   } catch (err) {
