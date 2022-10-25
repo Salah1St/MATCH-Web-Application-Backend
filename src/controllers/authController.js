1964c843003b745ef09d34eaae910340b187e19f
Sarun <sarunz488817@gmail.com>
2022-10-25 15:30:03 +0700

    fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,16 +1,16 @@
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const cloudinary = require("../utils/cloudinary");
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const cloudinary = require('../utils/cloudinary');
 
 // const { Op } = require("sequelize");
 
-const AppError = require("../utils/appError");
-const { User } = require("../sequelize/models");
+const AppError = require('../utils/appError');
+const { User } = require('../sequelize/models');
 
 const genToken = (payload) =>
-  jwt.sign(payload, process.env.JWT_SECRET_KEY || "private_key", {
-    expiresIn: process.env.JWT_EXPIRES || "1d",
+  jwt.sign(payload, process.env.JWT_SECRET_KEY || 'private_key', {
+    expiresIn: process.env.JWT_EXPIRES || '1d',
   });
 
 exports.register = async (req, res, next) => {
@@ -27,42 +27,42 @@ exports.register = async (req, res, next) => {
     } = req.body;
     console.log(req.body);
     if (!username) {
-      throw new AppError("username is required", 400);
+      throw new AppError('username is required', 400);
     }
 
     if (!gender) {
-      throw new AppError("gender is required", 400);
+      throw new AppError('gender is required', 400);
     }
 
     if (!emailOrMobile) {
-      throw new AppError("email address or mobile is required", 400);
+      throw new AppError('email address or mobile is required', 400);
     }
 
     if (!password) {
-      throw new AppError("password is required", 400);
+      throw new AppError('password is required', 400);
     }
 
     if (password !== confirmPassword) {
-      throw new AppError("password and confirm password did not match", 400);
+      throw new AppError('password and confirm password did not match', 400);
     }
 
     if (!firstName) {
-      throw new AppError("firstName is required", 400);
+      throw new AppError('firstName is required', 400);
     }
 
     if (!lastName) {
-      throw new AppError("lastName is required", 400);
+      throw new AppError('lastName is required', 400);
     }
 
     if (!birthDate) {
-      throw new AppError("birthdate is required", 400);
+      throw new AppError('birthdate is required', 400);
     }
 
-    const isEmail = validator.isEmail(emailOrMobile + "");
-    const isMobile = validator.isMobilePhone(emailOrMobile + "");
+    const isEmail = validator.isEmail(emailOrMobile + '');
+    const isMobile = validator.isMobilePhone(emailOrMobile + '');
 
     if (!isEmail && !isMobile) {
-      throw new AppError("email address or mobile is invalid format", 400);
+      throw new AppError('email address or mobile is invalid format', 400);
     }
 
     // const isDate = validator.isDate(birthDate + "");
@@ -84,7 +84,7 @@ exports.register = async (req, res, next) => {
 
     const userInfo = await User.findOne({
       where: user.id,
-      attributes: { exclude: "password" },
+      attributes: { exclude: 'password' },
     });
     console.log(userInfo);
 
@@ -99,18 +99,18 @@ exports.login = async (req, res, next) => {
   try {
     const { username, password } = req.body;
 
-    if (typeof username !== "string" || typeof password !== "string") {
-      throw new AppError("username or password is invalid", 400);
+    if (typeof username !== 'string' || typeof password !== 'string') {
+      throw new AppError('username or password is invalid', 400);
     }
 
     const user = await User.findOne({ where: { username: username } });
     if (!user) {
-      throw new AppError("not found user", 400);
+      throw new AppError('not found user', 400);
     }
 
     const isCorrect = await bcrypt.compare(password, user.password);
     if (!isCorrect) {
-      throw new AppError("password is invalid", 400);
+      throw new AppError('password is invalid', 400);
     }
 
     const token = genToken({ id: user.id });
@@ -125,10 +125,10 @@ exports.getMe = async (req, res) => {
     const user = req.user;
     const oneUser = await User.findOne({
       where: { id: user.id },
-      attributes: { exclude: "password" },
+      attributes: { exclude: 'password' },
       // include: { model: ShopPath }
     });
-    res.status(200).json({ oneUser });
+    res.status(200).json(oneUser);
   } catch (err) {
     next(err);
   }
