3081a6543c9ba7e73dbe0b54989b91b4546355d8
chadaniss <job.chadanis@gmail.com>
2022-10-27 18:38:58 +0700

    get location friend near me

--- src/controllers/nearmeController.js
+++ src/controllers/nearmeController.js
@@ -1,5 +1,6 @@
-const { Location } = require('../sequelize/models');
+const { Location, User } = require('../sequelize/models');
 const AppError = require('../utils/appError');
+const { Op } = require('sequelize');
 
 exports.updateLocation = async (req, res, next) => {
   try {
@@ -33,6 +34,34 @@ exports.updateLocation = async (req, res, next) => {
     }
 
     res.status(200).json({ message: 'Update location success' });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.friendLocation = async (req, res, next) => {
+  try {
+    const user = req.user;
+    const myLocation = await Location.findOne({ where: { userId: user.id } });
+    // console.log('latitude', myLocation.latitude);
+    // console.log('longitude', myLocation.longitude);
+    const MyLatitude = Math.trunc(myLocation.latitude * 10) / 10;
+    // console.log('MyLatitude', MyLatitude);
+    const MyLongitude = Math.trunc(myLocation.longitude * 10) / 10;
+    // console.log('MyLongitude', MyLongitude);
+    console.log('user.id', user.id);
+    const friendLocation = await Location.findAll({
+      where: {
+        id: {
+          [Op.ne]: user.id
+        },
+        latitude: { [Op.like]: `${MyLatitude}%` },
+        longitude: { [Op.like]: `${MyLongitude}%` }
+      },
+      include: { model: User, attributes: { exclude: 'password' } }
+    });
+
+    res.status(200).json(friendLocation);
   } catch (err) {
     next(err);
   }
