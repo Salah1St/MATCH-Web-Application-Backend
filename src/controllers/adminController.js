a7ae31a7a08fce6dd82d750db76f717eacc89f25
Basbusyy <Pasit.Jeamrattanasakul@gmail.com>
2022-10-31 17:16:34 +0700

    EditController

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -1,19 +1,19 @@
-const cloudinary = require("../utils/cloudinary");
-const { Interest } = require("../sequelize/models");
-const AppError = require("../utils/appError");
-const fs = require("fs");
+const cloudinary = require('../utils/cloudinary');
+const { Interest } = require('../sequelize/models');
+const AppError = require('../utils/appError');
+const fs = require('fs');
 
 exports.createInterest = async (req, res, next) => {
   try {
     const user = req.user;
     const { text } = req.body;
 
-    if (user.role !== "admin") {
-      throw new AppError("you are not admin");
+    if (user.role !== 'admin') {
+      throw new AppError('you are not admin');
     }
 
     if (!req.file) {
-      throw new AppError("image is required", 400);
+      throw new AppError('image is required', 400);
     }
 
     const data = {};
@@ -44,17 +44,17 @@ exports.deleteInterest = async (req, res, next) => {
   try {
     const user = req.user;
     const { id } = req.params;
-    if (user.role !== "admin") {
-      throw new AppError("you are not admin", 400);
+    if (user.role !== 'admin') {
+      throw new AppError('you are not admin', 400);
     }
 
     const deleteInterest = await Interest.findOne({ where: { id: id } });
     if (!deleteInterest) {
-      throw new AppError("cannot delete product", 400);
+      throw new AppError('cannot delete product', 400);
     }
 
     await deleteInterest.destroy();
-    res.status(200).json({ message: "delete successfully" });
+    res.status(200).json({ message: 'delete successfully' });
   } catch (err) {
     next(err);
   }
@@ -62,10 +62,6 @@ exports.deleteInterest = async (req, res, next) => {
 
 exports.getAll = async (req, res, next) => {
   try {
-    const user = req.user;
-    if (user.role !== "admin") {
-      throw new AppError("you are not admin");
-    }
     const allInterest = await Interest.findAll();
     res.status(200).json({ allInterest: allInterest });
   } catch (err) {
@@ -76,13 +72,13 @@ exports.getAll = async (req, res, next) => {
 exports.getOne = async (req, res, next) => {
   try {
     const user = req.user;
-    if (user.role !== "admin") {
-      throw new AppError("you are not admin");
+    if (user.role !== 'admin') {
+      throw new AppError('you are not admin');
     }
     const { id } = req.params;
     const oneInterest = await Interest.findOne({ where: { id: id } });
     if (!oneInterest) {
-      throw new AppError("not found this id", 400);
+      throw new AppError('not found this id', 400);
     }
     res.status(200).json({ oneInterest: oneInterest });
   } catch (err) {
@@ -93,8 +89,8 @@ exports.getOne = async (req, res, next) => {
 exports.updateInterest = async (req, res, next) => {
   try {
     const user = req.user;
-    if (user.role !== "admin") {
-      throw new AppError("you are not admin");
+    if (user.role !== 'admin') {
+      throw new AppError('you are not admin');
     }
 
     const { text } = req.body;
@@ -116,7 +112,7 @@ exports.updateInterest = async (req, res, next) => {
     await Interest.update(
       {
         text,
-        icon,
+        icon
       },
       { where: { id } }
     );
