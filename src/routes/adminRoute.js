bc5d3eb21e18b0ca5724bc0adc33c33ef06ef7ec
chadaniss <job.chadanis@gmail.com>
2022-11-02 19:42:30 +0700

    fix updateInterest in admin controller

--- src/routes/adminRoute.js
+++ src/routes/adminRoute.js
@@ -1,28 +1,31 @@
-const express = require("express");
-const upload = require("../middlewares/upload");
+const express = require('express');
+const upload = require('../middlewares/upload');
 
-const adminController = require("../controllers/adminController");
-const authenticate = require("../middlewares/authenticate");
+const adminController = require('../controllers/adminController');
+const authenticate = require('../middlewares/authenticate');
 
 const router = express.Router();
 
 router
-  .route("/createInterest")
-  .post(upload.single("icon"), authenticate, adminController.createInterest);
+  .route('/createInterest')
+  .post(upload.single('icon'), authenticate, adminController.createInterest);
 
-router.get("/getAll", authenticate, adminController.getAll);
-router.get("/getOne/:id", authenticate, adminController.getOne);
+router.get('/getAll', authenticate, adminController.getAll);
+router.get('/getOne/:id', authenticate, adminController.getOne);
 
 router.delete(
-  "/deleteInterest/:id",
+  '/deleteInterest/:id',
   authenticate,
   adminController.deleteInterest
 );
 
 router.patch(
-  "/updateInterest/:id",
+  '/updateInterest/:id',
   authenticate,
-  upload.single("icon"),
+  upload.fields([
+    { name: 'icon', maxCount: 1 },
+    { name: 'interestImage', maxCount: 1 }
+  ]),
   adminController.updateInterest
 );
 
